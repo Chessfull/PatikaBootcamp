41b43b48b966986352cf01409069a16b6e1dbe72
Mert Topçu <merttopcu95@gmail.com>
2024-07-15 21:44:05 +0300

    Add files via upload

---
+++ patikaGreeting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patikaGreeting
+{
+    internal class patikaGreeting
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mert Topçu\nKendimi motive hissettiğim, tutku duyduğum bir alana profesyonel olarak yönelmek için Kodluyoruz'dan itibaren takip ettiğim Patika topluluğunda, emin ellerdeyim! ");
+            Console.ReadLine();        
+        }
+    }
+}
