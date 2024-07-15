dae9a21f08374ce7e161843c1fc4adcb6ae1fbc4
Mert Topçu <merttopcu95@gmail.com>
2024-07-15 22:00:01 +0300

    patikaGreeting.cs

--- patikaGreeting.cs
+++ patikaGreeting.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace patikaGreeting
+namespace PatikaGreeting
 {
-    internal class patikaGreeting
+    internal class PatikaGreetingTask
     {
         static void Main(string[] args)
         {
