7bd4809e5f7580443f5bf072e93bcd184a37f9d4
Mert Topçu <merttopcu95@gmail.com>
2024-07-16 14:46:40 +0300

    MainFile.cs

--- MainFile.cs
+++ MainFile.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System; //Only need
 
 namespace Practises
 {
@@ -10,6 +6,10 @@ namespace Practises
     {
         static void Main(string[] args)
         {
+            //Patika Greeting
+            PatikaGreeting greeting = new PatikaGreeting();
+            greeting.Task();
+
             //Conditionals Practice
             ConditionalsPractice conditionalsPractice = new ConditionalsPractice();
             conditionalsPractice.Task();
