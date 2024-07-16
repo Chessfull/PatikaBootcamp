da6f9131950b5faea26608207d1231cba6f1a578
Mert Topçu <merttopcu95@gmail.com>
2024-07-16 14:43:03 +0300

    Add files via upload

---
+++ MainFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practises
+{
+    internal class MainFile
+    {
+        static void Main(string[] args)
+        {
+            //Conditionals Practice
+            ConditionalsPractice conditionalsPractice = new ConditionalsPractice();
+            conditionalsPractice.Task();
+
+            //Switch Practice
+            SwitchPractise switchPractise = new SwitchPractise();
+            switchPractise.Task();
+
+            //For Practise
+            ForLoopPractises forLoopPractises = new ForLoopPractises();
+            forLoopPractises.Task();
+
+            //Mini Quiz Program
+            QuizProgramPractise quizProgramPractise = new QuizProgramPractise();
+            quizProgramPractise.Task();
+        }
+    }
+}
